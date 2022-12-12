72fa31afd5fd9b5b6abf6bf2ab880b80a0d6867b
CGJI58 <leemsk22@naver.com>
2022-12-12 22:59:30 +0900

    ~#6.5

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#fbc531",
+};
+export const lightTheme: DefaultTheme = {
+  bgColor: "#f5f6fa",
+  textColor: "#2f3640",
+  accentColor: "#fbc531",
+};
