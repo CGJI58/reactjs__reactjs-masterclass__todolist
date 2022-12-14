f6c858298a71ebaa1c4180a9d695fc05e45890c4
CGJI58 <leemsk22@naver.com>
2022-12-14 17:25:21 +0900

    ~#6.7

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,17 +2,32 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch);
+  const { register, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
   return (
     <div>
-      <form>
-        <input {...register("email")} placeholder="email" />
+      <form onSubmit={handleSubmit(onValid)}>
+        <input {...register("email", { required: true })} placeholder="email" />
         <input {...register("firstName")} placeholder="firstName" />
         <input {...register("lastName")} placeholder="lastName" />
-        <input {...register("userName")} placeholder="UserName" />
+        <input
+          {...register("userName", { required: true, minLength: 10 })}
+          placeholder="UserName"
+        />
         <input {...register("password")} placeholder="password" />
-        <input {...register("passwordConfirm")} placeholder="passwordConfirm" />
+        <input
+          {...register("passwordConfirm", {
+            required: true,
+            minLength: {
+              value: 5,
+              message: "password length should be longer than 5.",
+            },
+          })}
+          placeholder="passwordConfirm"
+        />
         <button>Add</button>
       </form>
     </div>
