afec2bd3f24cdf263ab8f81060796e0008d35e8c
CGJI58 <leemsk22@naver.com>
2022-12-23 17:16:16 +0900

    ~#7.4

--- src/theme.ts
+++ src/theme.ts
@@ -1,12 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "#f5f6fa",
-  accentColor: "#fbc531",
-};
-export const lightTheme: DefaultTheme = {
-  bgColor: "#f5f6fa",
-  textColor: "#2f3640",
-  accentColor: "#fbc531",
+  bgColor: "#3F8CF2",
+  boardColor: "#DADFE9",
+  cardColor: "white",
 };
