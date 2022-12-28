03d6b5922da46169d114a61a44fbe445e5fea40b
CGJI58 <leemsk22@naver.com>
2022-12-28 13:47:10 +0900

    ~#7.5

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,55 +1,7 @@
 import { atom, selector } from "recoil";
 import { recoilPersist } from "recoil-persist";
-export enum Categories {
-  "TO_DO" = "TO_DO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-}
 
-export interface IToDo {
-  text: string;
-  id: number;
-  category: Categories;
-}
-
-const { persistAtom } = recoilPersist({
-  key: "toDosLocal",
-  storage: localStorage,
-});
-
-export const categoryState = atom<Categories>({
-  key: "category",
-  default: Categories.TO_DO,
-});
-
-export const toDoState = atom<IToDo[]>({
+export const toDoState = atom({
   key: "toDo",
-  default: [],
-  effects_UNSTABLE: [persistAtom],
-});
-
-export const toDoSelector = selector({
-  key: "toDoSelector",
-  get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos?.filter((toDo) => toDo.category === category);
-  },
-});
-
-export const minutesState = atom({
-  key: "minute",
-  default: 0,
-});
-
-export const hourSelector = selector({
-  key: "hours",
-  get: ({ get }) => {
-    const minutes = get(minutesState);
-    return minutes / 60;
-  },
-  set: ({ set }, newHour) => {
-    const minutes = Number(newHour) * 60;
-    set(minutesState, minutes);
-  },
+  default: ["a", "b", "c", "d", "e", "f"],
 });
